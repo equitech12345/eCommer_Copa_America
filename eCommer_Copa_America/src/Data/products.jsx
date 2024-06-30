1dd08841139e1169756b6674cfe4fdb48fa881c1
Juan Balladarez <juan.balladarez@gmail.com>
2024-06-30 15:44:34 +0100

    Se crea la lista de productos

---
+++ eCommer_Copa_America/src/Data/products.jsx
@@ -0,0 +1,65 @@
+export const items = [
+    {
+      "Id": "1",
+      "Name": "Camiseta Oficial Argentina",
+      "Description": "Camiseta oficial de la selección argentina de fútbol, temporada 2024.",
+      "Photo": "images/camiseta-argentina.jpg",
+      "Price": 89.99
+    },
+    {
+      "id": "2",
+      "Name": "Balón de Fútbol Argentina",
+      "Description": "Balón de fútbol con el diseño de la selección argentina.",
+      "Photo": "images/balon-argentina.jpg",
+      "Price": 29.99
+    },
+    {
+      "Id": "3",
+      "Name": "Bufanda Argentina",
+      "Description": "Bufanda de la selección argentina de fútbol, ideal para los días de partido.",
+      "Photo": "images/bufanda-argentina.jpg",
+      "Price": 19.99
+    },
+    {
+      "Id": "4",
+      "Name": "Gorra Argentina",
+      "Description": "Gorra con el escudo de la selección argentina de fútbol.",
+      "Photo": "images/gorra-argentina.jpg",
+      "Price": 24.99
+    },
+    {
+      "Id": "5",
+      "Name": "Botella de Agua Argentina",
+      "Description": "Botella de agua reutilizable con el logo de la selección argentina.",
+      "Photo": "images/botella-argentina.jpg",
+      "Price": 14.99
+    },
+    {
+      "Id": "6",
+      "Name": "Bandera Argentina",
+      "Description": "Bandera oficial de la selección argentina de fútbol.",
+      "Photo": "images/bandera-argentina.jpg",
+      "Price": 12.99
+    },
+    {
+      "Id": "8",
+      "Name": "Chaqueta Argentina",
+      "Description": "Chaqueta deportiva de la selección argentina de fútbol.",
+      "Photo": "images/chaqueta-argentina.jpg",
+      "Price": 69.99
+    },
+    {
+      "Id": "9",
+      "Name": "Calcetines Argentina",
+      "Description": "Calcetines con el diseño de la selección argentina de fútbol.",
+      "Photo": "images/calcetines-argentina.jpg",
+      "Price": 9.99
+    },
+    {
+      "Id": "10",
+      "Name": "Pantalones Cortos Argentina",
+      "Description": "Pantalones cortos oficiales de la selección argentina de fútbol.",
+      "Photo": "images/pantalones-argentina.jpg",
+      "Price": 34.99
+    }
+  ]
