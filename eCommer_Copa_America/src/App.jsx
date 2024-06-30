07eafd937d2ceaac2917c6dc87404b501cc1ef08
Juan Balladarez <juan.balladarez@gmail.com>
2024-06-30 13:38:32 +0100

    Cramos el proyecto

---
+++ eCommer_Copa_America/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <header>
+      <h1>eCommer Copa Ameria</h1>
+      <p>Bienvenido al eComer de la Selección Argentina</p>
+
+      
+    </header>
+      
+    </>
+  )
+}
+
+export default App
