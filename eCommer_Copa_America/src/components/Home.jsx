a428e379ce778a8cef84f098fd3de2f12692a8a6
Juan Balladarez <juan.balladarez@gmail.com>
2024-06-30 15:40:34 +0100

    Se crea el componente Home y sus estilos

--- eCommer_Copa_America/src/components/Home.jsx
+++ eCommer_Copa_America/src/components/Home.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { useState } from 'react';
+import './Home.css'
+
 
 function Home() {
+  const countries = ['Argentina', 'Brasil', 'Chile', 'México', 'Perú'];
+    
+  const handleSelectCountry = (country) => {
+    alert('Seleccionaste ' + country);
+  };
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+    <h2>Selecciona un país:</h2>
+    <ul>
+      {countries.map((country) => (
+        <p className='p_country'
+          key={country}
+          onClick={() => handleSelectCountry(country)}
+        >
+          {country}
+        </p>
+      ))}
+    </ul>
+  </div>
+);
+};
 
 export default Home
+
