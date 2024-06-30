ba1584f4bb5d97e3b4c0a931c8a492a102b696b1
Juan Balladarez <juan.balladarez@gmail.com>
2024-06-30 14:43:33 +0100

    Se crea el componente register y sus estilos

--- eCommer_Copa_America/src/components/Register.jsx
+++ eCommer_Copa_America/src/components/Register.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import './Register.css'
 
 function Register() {
+    const handleSubmit = (e) => {
+     };
   return (
-    <div>Register</div>
+    <>
+        <h1>Register</h1>
+        <form onSubmit={handleSubmit} className='form_register'>
+            <label>
+                Name
+            <input type="text"  placeholder="Name"/>
+            </label>
+            <label>Last Name
+            <input type="text" placeholder="Last Name"/>
+            </label>
+            <label>Email
+            <input type="email" placeholder="Email" name='email'/>
+            </label>
+            <label>Password
+            <input type="password" placeholder="Password" name='password'/>
+            </label>
+            <button type="submit" className='button_register'>Register</button>    
+        </form>
+    </>
   )
 }
 
