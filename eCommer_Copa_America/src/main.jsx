da15ed98a2ef6aea9801a451b8ffa830079f52bd
Juan Balladarez <juan.balladarez@gmail.com>
2024-06-30 13:59:29 +0100

    Nuevo Componente NotFound

--- eCommer_Copa_America/src/main.jsx
+++ eCommer_Copa_America/src/main.jsx
@@ -6,6 +6,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './components/Home.jsx'
 import Login from './components/Login.jsx'
 import Register from './components/Register.jsx'
+import NotFound from './components/NotFound.jsx'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -14,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />}>
          <Route index element={<Home />} />
-        <Route path="*" element="Not Found" />
+        <Route path="*" element={<NotFound/>} />
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
