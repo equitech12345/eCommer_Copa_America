0a6df4ee734573a9f9cd3641e0e7952d32136ba1
Juan Balladarez <juan.balladarez@gmail.com>
2024-06-30 13:44:24 +0100

    Creamos los componentes login, register y home

--- eCommer_Copa_America/src/App.jsx
+++ eCommer_Copa_America/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { NavLink } from 'react-router-dom'
 
 function App() {
 
@@ -7,8 +8,15 @@ function App() {
     <header>
       <h1>eCommer Copa Ameria</h1>
       <p>Bienvenido al eComer de la Selección Argentina</p>
+    
+    <nav>
+    <NavLink to ={"/"}>Home</NavLink>
+    <NavLink to ={"login"}>Login</NavLink>
+    <NavLink to ={"register"}>Register</NavLink>
 
-      
+    </nav>
+
+
     </header>
       
     </>
