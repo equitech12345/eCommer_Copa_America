93a1c16f1f18d885951d2e333288bc9155524bbe
Juan Balladarez <juan.balladarez@gmail.com>
2024-07-06 20:00:58 +0100

    estilos

--- eCommer_Copa_America/src/App.jsx
+++ eCommer_Copa_America/src/App.jsx
@@ -9,11 +9,15 @@ function App() {
       <h1>eCommer Copa America</h1>
       <p>Bienvenido al eComer de la Selección Argentina</p>
     
-    <nav>
-    <NavLink to ={"/"}>Home</NavLink>
-    <NavLink to ={"login"}>Login</NavLink>
-    <NavLink to ={"register"}>Register</NavLink>
-    <NavLink to ={"items"}>Items</NavLink>
+    <nav className='navbar'>
+    <NavLink className={({isActive})=>
+    isActive? 'navbar-selected' : null} to ={"/"}>Home</NavLink>
+    <NavLink className={({isActive})=>
+    isActive? 'navbar-selected' : null} to ={"login"}>Login</NavLink>
+    <NavLink className={({isActive})=>
+    isActive? 'navbar-selected' : null} to ={"register"}>Register</NavLink>
+    <NavLink className={({isActive})=>
+    isActive? 'navbar-selected' : null} to ={"items"}>Items</NavLink>
     </nav>
     <Outlet />
 
