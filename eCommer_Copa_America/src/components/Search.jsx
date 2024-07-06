e464b142fc4e182208d48ee4af0b2843529b345e
Juan Balladarez <juan.balladarez@gmail.com>
2024-07-06 19:25:44 +0100

    estilos

--- eCommer_Copa_America/src/components/Search.jsx
+++ eCommer_Copa_America/src/components/Search.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
 function Search({setQuery}) {
-  return (
-    <form>
-        <input 
-        type="search" 
-        name='search' 
-        aria-label='Producto a buscar' 
-        placeholder='Producto a buscar...' 
-        onChange={(e)=> setQuery(e.target.value)}/>
+return (
+    <form >
+            <input style={{ backgroundColor: 'rgba(203, 232, 241, 0.6)' }} 
+            type="search" 
+            name='search' 
+            aria-label='Producto a buscar' 
+            placeholder='Producto a buscar...' 
+            onChange={(e)=> setQuery(e.target.value)}/>
     </form>
-  )
+)
 }
 
 export default Search
